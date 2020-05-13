01d5d4d3341caca3edfc78edcf23b58a259e8ab8
ronpe <ron.peretz1@gmail.com>
2020-05-14 02:11:21 +0300

    Edit middleware to handle with '/' and '/users'

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use( (res, req, next) => {
-    console.log('I am the first middleware');
-    next();
+app.use('/users' ,(req, res, next) => {
+    res.send('<h1>Users Page</h1>');
 });
 
 app.use((req, res, next) => {
-    console.log('I am the second middleware');
-    res.send('<h1>Assignment 2 - Express.js</h1>')
+    res.send('<h1>Main Page</h1>')
 })
 
 app.listen(3000);
