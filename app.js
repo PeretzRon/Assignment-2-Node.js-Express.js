d4a232362cba8a79dcf4a2e4a48646ad226d318a
ronpe <ron.peretz1@gmail.com>
2020-05-14 02:08:28 +0300

    Adding 2 middleware

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use('/',(res, req, next) => {
-    console.log(req); // just to test the server
+app.use( (res, req, next) => {
+    console.log('I am the first middleware');
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log('I am the second middleware');
+    res.send('<h1>Assignment 2 - Express.js</h1>')
+})
 
 app.listen(3000);
